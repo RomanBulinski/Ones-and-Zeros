2c28e946313665d12c9448ae888ed5c5a4dfba42
Roman Bulinski <aboandrus@gmail.com>
2019-05-23 08:26:09 +0200

    Created test class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
 import java.util.List;
 
+import static java.lang.Math.pow;
+
 public class Main {
 
     public static int ConvertBinaryArrayToInt(List<Integer> binary) {
-        // Your Code
+
+        int result=0;
+        for(int i =1; i<binary.size(); i++){
+            if(binary[i]==0){
+                result = result + pow(1,i);
+            }
+        }
     }
 
 }
+
+
+
+
+
+//15 -> 1111
